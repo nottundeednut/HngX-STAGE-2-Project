92de57483f5725864b2e5d84674e4768de91d153
Babatunde Owolabi <chatwittundeowolabi@gmail.com>
2023-09-13 04:29:07 +0100

    created row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
-
   return (
-    <>
-  
-    </>
-  )
+    <div>
+      <h1>Testing</h1>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Popular" fetchUrl={requests.fetchPopular} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/requests.jsx
+++ src/requests.jsx
@@ -1,9 +1,8 @@
 const API_KEY = "4eee2347d333e477301d412f11461220";
 
 const requests = {
-    fetchPopular: `
-    https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
-    fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`,
-}
+  fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+};
 
 export default requests;
